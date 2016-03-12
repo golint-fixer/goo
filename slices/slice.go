0478191c48c9189eb811f34b6314aca49852999f
Will Faught <will.faught@gmail.com>
2016-03-12 01:27:38 -0800

    Add Slice

---
+++ slices/slice.go
@@ -0,0 +1,23 @@
+package slices
+
+type Slice interface {
+	Append(v ...interface{}) Slice
+
+	AppendSlice(s Slice) Slice
+
+	Copy(s Slice) int
+
+	Get(i int) interface{}
+
+	Len() int
+
+	Make(l, c int) Slice
+
+	Set(i int, v interface{})
+
+	Slice(i, j int) Slice
+
+	SliceCap(i, j, m int) Slice
+
+	Zero() interface{}
+}
