906f8a9e335dec607f4871dcd5d68917ecaa0fb5
Will Faught <will.faught@gmail.com>
2016-03-13 01:52:45 -0800

    Add chans.Iterator

--- chans/chan.go
+++ chans/chan.go
@@ -1,5 +1,7 @@
 package chans
 
+import "github.com/willfaught/lang"
+
 type Chan interface {
 	Cap() int
 
@@ -18,4 +20,39 @@ type Chan interface {
 	ReceiveCheck() (interface{}, bool)
 
 	Send(v interface{})
+}
+
+func Iterator(c ChanReceive) lang.Iterator {
+	return &iterator{c: c}
+}
+
+type iterator struct {
+	c ChanReceive
+
+	checked bool
+
+	ok bool
+
+	v interface{}
+}
+
+func (i *iterator) More() bool {
+	if i.checked {
+		return i.ok
+	}
+
+	i.v, i.ok = i.c.ReceiveCheck()
+	i.checked = true
+
+	return i.ok
+}
+
+func (i *iterator) Next() interface{} {
+	if !i.checked {
+		i.More()
+	}
+
+	i.checked = false
+
+	return i.v
 }
