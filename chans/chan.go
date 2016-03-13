0787fd23021125236ffd2e633280fede870c4683
Will Faught <will.faught@gmail.com>
2016-03-13 03:02:13 -0700

    Style

--- chans/chan.go
+++ chans/chan.go
@@ -1,7 +1,5 @@
 package chans
 
-import "github.com/willfaught/lang"
-
 type Chan interface {
 	Cap() int
 
@@ -29,38 +27,3 @@ var (
 	_ Chan = ChanRune(nil)
 	_ Chan = ChanString(nil)
 )
-
-func Iterator(c ChanReceive) lang.Iterator {
-	return &iterator{c: c}
-}
-
-type iterator struct {
-	c ChanReceive
-
-	checked bool
-
-	ok bool
-
-	v interface{}
-}
-
-func (i *iterator) More() bool {
-	if i.checked {
-		return i.ok
-	}
-
-	i.v, i.ok = i.c.ReceiveCheck()
-	i.checked = true
-
-	return i.ok
-}
-
-func (i *iterator) Next() interface{} {
-	if !i.checked {
-		i.More()
-	}
-
-	i.checked = false
-
-	return i.v
-}
--- chans/chan_receive.go
+++ chans/chan_receive.go
@@ -1,5 +1,7 @@
 package chans
 
+import "github.com/willfaught/lang"
+
 type ChanReceive interface {
 	Cap() int
 
@@ -10,4 +12,39 @@ type ChanReceive interface {
 	Receive() interface{}
 
 	ReceiveCheck() (interface{}, bool)
+}
+
+func Iterator(c ChanReceive) lang.Iterator {
+	return &iterator{c: c}
+}
+
+type iterator struct {
+	c ChanReceive
+
+	checked bool
+
+	ok bool
+
+	v interface{}
+}
+
+func (i *iterator) More() bool {
+	if i.checked {
+		return i.ok
+	}
+
+	i.v, i.ok = i.c.ReceiveCheck()
+	i.checked = true
+
+	return i.ok
+}
+
+func (i *iterator) Next() interface{} {
+	if !i.checked {
+		i.More()
+	}
+
+	i.checked = false
+
+	return i.v
 }
