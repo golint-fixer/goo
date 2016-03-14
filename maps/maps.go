78251f7953d734c8c3abd115e03fac17ff2249ff
Will Faught <will.faught@gmail.com>
2016-03-14 14:44:49 -0700

    Add MapInterfaceBool

--- maps/maps.go
+++ maps/maps.go
@@ -1,5 +1,53 @@
 package maps
 
+type MapInterfaceBool map[interface{}]bool
+
+func (m MapInterfaceBool) Delete(k interface{}) {
+	delete(m, k)
+}
+
+func (m MapInterfaceBool) Get(k interface{}) interface{} {
+	return m[k]
+}
+
+func (m MapInterfaceBool) GetCheck(k interface{}) (interface{}, bool) {
+	var v, ok = m[k]
+
+	return v, ok
+}
+
+func (m MapInterfaceBool) KeyValues() [][2]interface{} {
+	var kvs [][2]interface{}
+
+	for k, v := range m {
+		kvs = append(kvs, [2]interface{}{k, v})
+	}
+
+	return kvs
+}
+
+func (m MapInterfaceBool) Keys() []interface{} {
+	var ks []interface{}
+
+	for k := range m {
+		ks = append(ks, k)
+	}
+
+	return ks
+}
+
+func (m MapInterfaceBool) Len() int {
+	return len(m)
+}
+
+func (m MapInterfaceBool) Make(c int) Map {
+	return make(MapInterfaceBool, c)
+}
+
+func (m MapInterfaceBool) Set(k, v interface{}) {
+	m[k] = v.(bool)
+}
+
 type MapInterfaceStruct map[interface{}]struct{}
 
 func (m MapInterfaceStruct) Delete(k interface{}) {
