0e14154861bf2e92f081d0f9848054e115cc77a3
Will Faught <will.faught@gmail.com>
2016-03-14 14:48:24 -0700

    Add MapInterfaceInt/Interface

--- maps/maps.go
+++ maps/maps.go
@@ -48,6 +48,102 @@ func (m MapInterfaceBool) Set(k, v interface{}) {
 	m[k] = v.(bool)
 }
 
+type MapInterfaceInt map[interface{}]int
+
+func (m MapInterfaceInt) Delete(k interface{}) {
+	delete(m, k)
+}
+
+func (m MapInterfaceInt) Get(k interface{}) interface{} {
+	return m[k]
+}
+
+func (m MapInterfaceInt) GetCheck(k interface{}) (interface{}, bool) {
+	var v, ok = m[k]
+
+	return v, ok
+}
+
+func (m MapInterfaceInt) KeyValues() [][2]interface{} {
+	var kvs [][2]interface{}
+
+	for k, v := range m {
+		kvs = append(kvs, [2]interface{}{k, v})
+	}
+
+	return kvs
+}
+
+func (m MapInterfaceInt) Keys() []interface{} {
+	var ks []interface{}
+
+	for k := range m {
+		ks = append(ks, k)
+	}
+
+	return ks
+}
+
+func (m MapInterfaceInt) Len() int {
+	return len(m)
+}
+
+func (m MapInterfaceInt) Make(c int) Map {
+	return make(MapInterfaceInt, c)
+}
+
+func (m MapInterfaceInt) Set(k, v interface{}) {
+	m[k] = v.(int)
+}
+
+type MapInterfaceInterface map[interface{}]interface{}
+
+func (m MapInterfaceInterface) Delete(k interface{}) {
+	delete(m, k)
+}
+
+func (m MapInterfaceInterface) Get(k interface{}) interface{} {
+	return m[k]
+}
+
+func (m MapInterfaceInterface) GetCheck(k interface{}) (interface{}, bool) {
+	var v, ok = m[k]
+
+	return v, ok
+}
+
+func (m MapInterfaceInterface) KeyValues() [][2]interface{} {
+	var kvs [][2]interface{}
+
+	for k, v := range m {
+		kvs = append(kvs, [2]interface{}{k, v})
+	}
+
+	return kvs
+}
+
+func (m MapInterfaceInterface) Keys() []interface{} {
+	var ks []interface{}
+
+	for k := range m {
+		ks = append(ks, k)
+	}
+
+	return ks
+}
+
+func (m MapInterfaceInterface) Len() int {
+	return len(m)
+}
+
+func (m MapInterfaceInterface) Make(c int) Map {
+	return make(MapInterfaceInterface, c)
+}
+
+func (m MapInterfaceInterface) Set(k, v interface{}) {
+	m[k] = v.(int)
+}
+
 type MapInterfaceStruct map[interface{}]struct{}
 
 func (m MapInterfaceStruct) Delete(k interface{}) {
