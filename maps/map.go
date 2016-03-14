24e00de76516e225a6304d71b78d800788795acc
Will Faught <will.faught@gmail.com>
2016-03-14 14:29:58 -0700

    Add maps.Keys/KeyValuesIterator

--- maps/map.go
+++ maps/map.go
@@ -1,5 +1,7 @@
 package maps
 
+import "github.com/willfaught/lang"
+
 type Map interface {
 	Delete(k interface{})
 
@@ -16,4 +18,56 @@ type Map interface {
 	Make(c int) Map
 
 	Set(k, v interface{})
+}
+
+func KeyValuesIterator(m Map) lang.Iterator {
+	var kv = m.KeyValues()
+
+	return &keyValuesIterator{n: len(kv), s: kv}
+}
+
+func KeysIterator(m Map) lang.Iterator {
+	var k = m.Keys()
+
+	return &keyIterator{n: len(k), s: k}
+}
+
+type keyIterator struct {
+	i int
+
+	n int
+
+	s []interface{}
+}
+
+func (i keyIterator) More() bool {
+	return i.i < i.n
+}
+
+func (i *keyIterator) Next() interface{} {
+	var v = i.s[i.i]
+
+	i.i++
+
+	return v
+}
+
+type keyValuesIterator struct {
+	i int
+
+	n int
+
+	s [][2]interface{}
+}
+
+func (i keyValuesIterator) More() bool {
+	return i.i < i.n
+}
+
+func (i *keyValuesIterator) Next() interface{} {
+	var v = i.s[i.i]
+
+	i.i++
+
+	return v
 }
