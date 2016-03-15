74a80de4440c601337ff6807988a206f36e55ba9
Will Faught <will.faught@gmail.com>
2016-03-14 18:28:12 -0700

    Add lang.Repeat/Replicate

--- iterator.go
+++ iterator.go
@@ -5,3 +5,41 @@ type Iterator interface {
 
 	Next() interface{}
 }
+
+func Repeat(v interface{}) Iterator {
+	return repeater{v: v}
+}
+
+func Replicate(v interface{}, n int) Iterator {
+	return &replicator{n: n, v: v}
+}
+
+type repeater struct {
+	v interface{}
+}
+
+func (r repeater) More() bool {
+	return true
+}
+
+func (r repeater) Next() interface{} {
+	return r.v
+}
+
+type replicator struct {
+	i int
+
+	n int
+
+	v interface{}
+}
+
+func (r replicator) More() bool {
+	return r.i < r.n
+}
+
+func (r *replicator) Next() interface{} {
+	r.i++
+
+	return r.v
+}
