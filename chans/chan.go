9db7f2620a9755e2688c8da01a0a37290f1446a5
Will Faught <will.faught@gmail.com>
2016-05-09 12:41:47 -0700

    Add chans funcs

--- chans/chan.go
+++ chans/chan.go
@@ -1,5 +1,7 @@
 package chans
 
+import "sync"
+
 type Chan interface {
 	Cap() int
 
@@ -28,7 +30,240 @@ var (
 	_ Chan = ChanString(nil)
 )
 
-// TODO: Branch, Combine, Compose, Copy, CopyPrimSlice, Merge, Replay
+func Branch(c ChanReceive, n int) []ChanReceive {
+	var rs, ss = make([]ChanReceive, n), make([]ChanSend, n)
+
+	for i := 0; i < n; i++ {
+		var b = c.Make(c.Cap())
+
+		rs[i], ss[i] = b, b
+	}
+
+	go func() {
+		for i := 0; ; i++ {
+			var v, ok = c.ReceiveCheck()
+
+			if !ok {
+				break
+			}
+
+			ss[i%n].Send(v)
+		}
+
+		for i := 0; i < n; i++ {
+			ss[i].Close()
+		}
+	}()
+
+	return rs
+}
+
+func Broadcast(c ...ChanSend) ChanSend {
+	var broadcast = c[0].Make(c[0].Cap())
+
+	go func() {
+		for {
+			var v, ok = broadcast.ReceiveCheck()
+
+			if !ok {
+				break
+			}
+
+			for _, c := range c {
+				c.Send(v)
+			}
+		}
+
+		for _, c := range c {
+			c.Close()
+		}
+	}()
+
+	return broadcast
+}
+
+func Compose(r ChanReceive, s ChanSend) Chan {
+	var c = r.Make(s.Cap())
+
+	go func() {
+		var v, ok = r.ReceiveCheck()
+
+		if !ok {
+			c.Close()
+
+			return
+		}
+
+		c.Send(v)
+	}()
+
+	go func() {
+		var v, ok = c.ReceiveCheck()
+
+		if !ok {
+			s.Close()
+
+			return
+		}
+
+		s.Send(v)
+	}()
+
+	return c
+}
+
+func Copy(c ChanReceive, n int) []ChanReceive {
+	var rs []ChanReceive
+	var ss []ChanSend
+	var ns []chan struct{}
+	var vs []interface{}
+	var done bool
+	var vsm, donem sync.Mutex
+
+	for i := 0; i < n; i++ {
+		var copy = c.Make(c.Cap())
+
+		rs = append(rs, copy)
+		ss = append(ss, copy)
+		ns = append(ns, make(chan struct{}, 1))
+
+		go func() {
+			var j int
+
+			for {
+				donem.Lock()
+				var d = done
+				donem.Unlock()
+
+				if d {
+					break
+				}
+
+				<-ns[i]
+
+				vsm.Lock()
+				var s = vs[j:len(vs)]
+				vsm.Unlock()
+
+				for _, v := range s {
+					ss[i].Send(v)
+				}
+
+				j += len(s)
+			}
+
+			ss[i].Close()
+		}()
+	}
+
+	go func() {
+		for {
+			var v, ok = c.ReceiveCheck()
+
+			if !ok {
+				break
+			}
+
+			vsm.Lock()
+			vs = append(vs, v)
+			vsm.Unlock()
+
+			for _, n := range ns {
+				if len(n) == 0 {
+					n <- struct{}{}
+				}
+			}
+		}
+
+		donem.Lock()
+		done = true
+		donem.Unlock()
+	}()
+
+	return rs
+}
+
+func Cycle(c ChanReceive) ChanReceive {
+	var vs []interface{}
+	var cycle = make(ChanInterface, c.Cap())
+
+	go func() {
+		for {
+			var v, ok = c.ReceiveCheck()
+
+			if !ok {
+				break
+			}
+
+			vs = append(vs, v)
+			cycle.Send(v)
+		}
+
+		for {
+			for _, v := range vs {
+				cycle.Send(v)
+			}
+		}
+	}()
+
+	return cycle
+}
+
+func Merge(c ...ChanReceive) ChanReceive {
+	var merged = c[0].Make(0)
+	var w sync.WaitGroup
+
+	w.Add(len(c))
+
+	for i, l := 0, len(c); i < l; i++ {
+		go func(r ChanReceive) {
+			for {
+				var v, ok = r.ReceiveCheck()
+
+				if !ok {
+					break
+				}
+
+				merged.Send(v)
+			}
+
+			w.Done()
+		}(c[i])
+	}
+
+	go func() {
+		w.Wait()
+		merged.Close()
+	}()
+
+	return merged
+}
+
+func Repeat(v interface{}) ChanReceive {
+	var c = make(ChanInterface)
+
+	go func() {
+		for {
+			c.Send(v)
+		}
+	}()
+
+	return c
+}
+
+func Replicate(v interface{}, n int) ChanReceive {
+	var c = make(ChanInterface)
+
+	go func() {
+		for i := 0; i < n; i++ {
+			c.Send(v)
+		}
+
+		c.Close()
+	}()
+
+	return c
+}
 
 func Unblock(c ChanSend) ChanSend {
 	var unblocked = c.Make(0)
