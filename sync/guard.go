b9b436d6c660a975fbde1822e87e0bdea577e2b4
Will Faught <will.faught@gmail.com>
2016-09-09 22:12:00 -0700

    Add guard.go

---
+++ sync/guard.go
@@ -0,0 +1,36 @@
+package sync
+
+import "sync"
+
+type Guard struct {
+	i interface{}
+	m *sync.Mutex
+}
+
+func NewGuard(i interface{}) *Guard {
+	return &Guard{i: i, m: &sync.Mutex{}}
+}
+
+func (g *Guard) Get() interface{} {
+	g.m.Lock()
+
+	defer g.m.Unlock()
+
+	return g.i
+}
+
+func (g *Guard) Map(f func(interface{}) interface{}) {
+	g.m.Lock()
+
+	defer g.m.Unlock()
+
+	g.i = f(g.i)
+}
+
+func (g *Guard) Set(i interface{}) {
+	g.m.Lock()
+
+	defer g.m.Unlock()
+
+	g.i = i
+}
