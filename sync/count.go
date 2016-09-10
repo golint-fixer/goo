27d384c51a17313d5fd9685a2548a0b45a6fe95a
Will Faught <will.faught@gmail.com>
2016-09-09 22:11:50 -0700

    Add count.go

---
+++ sync/count.go
@@ -0,0 +1,44 @@
+package sync
+
+import "sync"
+
+type Count struct {
+	m *sync.Mutex
+	n int
+}
+
+func NewCount(n int) Guard {
+	return &Count{m: &sync.Mutex{}, n: n}
+}
+
+func (c *Count) Add(n int) {
+	c.m.Lock()
+
+	defer c.m.Unlock()
+
+	c.n += n
+}
+
+func (c *Count) Get() int {
+	c.m.Lock()
+
+	defer c.m.Unlock()
+
+	return c.n
+}
+
+func (c *Count) Set(n int) {
+	c.m.Lock()
+
+	defer c.m.Unlock()
+
+	c.n = n
+}
+
+func (c *Count) Sub(n int) {
+	c.m.Lock()
+
+	defer c.m.Unlock()
+
+	c.n -= n
+}
