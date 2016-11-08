17db84cfc8af6dafd531529171dc6bdbfb390046
Will Faught <will.faught@bigcommerce.com>
2016-11-08 00:15:05 -0800

    Update slice macro use of macro command

--- internal/slice/slice.go
+++ internal/slice/slice.go
@@ -1,71 +1,71 @@
 //go:generate go get github.com/willfaught/goo/cmd/goo
-//go:generate goo macro slice.go ../../slice_bool.go -d {"name":"Bool","sort":false,"element":"bool"}
-//go:generate goo macro slice.go ../../slice_byte.go -d {"name":"Byte","sort":true,"element":"byte"}
-//go:generate goo macro slice.go ../../slice_complex64.go -d {"name":"Complex64","sort":false,"element":"complex64"}
-//go:generate goo macro slice.go ../../slice_complex128.go -d {"name":"Complex128","sort":false,"element":"complex128"}
-//go:generate goo macro slice.go ../../slice_float32.go -d {"name":"Float32","sort":true,"element":"float32"}
-//go:generate goo macro slice.go ../../slice_float64.go -d {"name":"Float64","sort":true,"element":"float64"}
-//go:generate goo macro slice.go ../../slice_int.go -d {"name":"Int","sort":true,"element":"int"}
-//go:generate goo macro slice.go ../../slice_interface.go -d {"name":"Interface","sort":false,"element":"interface{}"}
-//go:generate goo macro slice.go ../../slice_int8.go -d {"name":"Int8","sort":true,"element":"int8"}
-//go:generate goo macro slice.go ../../slice_int16.go -d {"name":"Int16","sort":true,"element":"int16"}
-//go:generate goo macro slice.go ../../slice_int32.go -d {"name":"Int32","sort":true,"element":"int32"}
-//go:generate goo macro slice.go ../../slice_int64.go -d {"name":"Int64","sort":true,"element":"int64"}
-//go:generate goo macro slice.go ../../slice_rune.go -d {"name":"Rune","sort":true,"element":"rune"}
-//go:generate goo macro slice.go ../../slice_string.go -d {"name":"String","sort":true,"element":"string"}
-//go:generate goo macro slice.go ../../slice_uint.go -d {"name":"Uint","sort":true,"element":"uint"}
-//go:generate goo macro slice.go ../../slice_uintptr.go -d {"name":"Uintptr","sort":true,"element":"uintptr"}
-//go:generate goo macro slice.go ../../slice_uint8.go -d {"name":"Uint8","sort":true,"element":"uint8"}
-//go:generate goo macro slice.go ../../slice_uint16.go -d {"name":"Uint16","sort":true,"element":"uint16"}
-//go:generate goo macro slice.go ../../slice_uint32.go -d {"name":"Uint32","sort":true,"element":"uint32"}
-//go:generate goo macro slice.go ../../slice_uint64.go -d {"name":"Uint64","sort":true,"element":"uint64"}
+//go:generate goo macro slice.go ../../slice_bool.go -d {"Name":"Bool","Sort":false,"Element":"bool"}
+//go:generate goo macro slice.go ../../slice_byte.go -d {"Name":"Byte","Sort":true,"Element":"byte"}
+//go:generate goo macro slice.go ../../slice_complex64.go -d {"Name":"Complex64","Sort":false,"Element":"complex64"}
+//go:generate goo macro slice.go ../../slice_complex128.go -d {"Name":"Complex128","Sort":false,"Element":"complex128"}
+//go:generate goo macro slice.go ../../slice_float32.go -d {"Name":"Float32","Sort":true,"Element":"float32"}
+//go:generate goo macro slice.go ../../slice_float64.go -d {"Name":"Float64","Sort":true,"Element":"float64"}
+//go:generate goo macro slice.go ../../slice_int.go -d {"Name":"Int","Sort":true,"Element":"int"}
+//go:generate goo macro slice.go ../../slice_interface.go -d {"Name":"Interface","Sort":false,"Element":"interface{}"}
+//go:generate goo macro slice.go ../../slice_int8.go -d {"Name":"Int8","Sort":true,"Element":"int8"}
+//go:generate goo macro slice.go ../../slice_int16.go -d {"Name":"Int16","Sort":true,"Element":"int16"}
+//go:generate goo macro slice.go ../../slice_int32.go -d {"Name":"Int32","Sort":true,"Element":"int32"}
+//go:generate goo macro slice.go ../../slice_int64.go -d {"Name":"Int64","Sort":true,"Element":"int64"}
+//go:generate goo macro slice.go ../../slice_rune.go -d {"Name":"Rune","Sort":true,"Element":"rune"}
+//go:generate goo macro slice.go ../../slice_string.go -d {"Name":"String","Sort":true,"Element":"string"}
+//go:generate goo macro slice.go ../../slice_uint.go -d {"Name":"Uint","Sort":true,"Element":"uint"}
+//go:generate goo macro slice.go ../../slice_uintptr.go -d {"Name":"Uintptr","Sort":true,"Element":"uintptr"}
+//go:generate goo macro slice.go ../../slice_uint8.go -d {"Name":"Uint8","Sort":true,"Element":"uint8"}
+//go:generate goo macro slice.go ../../slice_uint16.go -d {"Name":"Uint16","Sort":true,"Element":"uint16"}
+//go:generate goo macro slice.go ../../slice_uint32.go -d {"Name":"Uint32","Sort":true,"Element":"uint32"}
+//go:generate goo macro slice.go ../../slice_uint64.go -d {"Name":"Uint64","Sort":true,"Element":"uint64"}
 
 package goo
 
-/// {{if .sort}}
+/// {{if .Sort}}
 import "sort" /// {{end}}
 
 /// {{if false}}
 type Slice interface{} /// {{end}}
 
 /// {{if false}}
-type __FIELD_element__ int /// {{end}}
+type __FIELD_Element__ int /// {{end}}
 
-// Slice__FIELD_name__ is a slice of __FIELD_element__.
-type Slice__FIELD_name__ []__FIELD_element__
+// Slice__FIELD_Name__ is a slice of __FIELD_Element__.
+type Slice__FIELD_Name__ []__FIELD_Element__
 
-var _ Slice = Slice__FIELD_name__(nil)
+var _ Slice = Slice__FIELD_Name__(nil)
 
-/// {{if .sort}}
-var _ sort.Interface = Slice__FIELD_name__(nil) /// {{end}}
+/// {{if .Sort}}
+var _ sort.Interface = Slice__FIELD_Name__(nil) /// {{end}}
 
 // Append appends v to s and returns the result.
-func (s Slice__FIELD_name__) Append(v ...interface{}) Slice {
+func (s Slice__FIELD_Name__) Append(v ...interface{}) Slice {
 	for _, v := range v {
-		s = append(s, v.(__FIELD_element__))
+		s = append(s, v.(__FIELD_Element__))
 	}
 
 	return s
 }
 
 // AppendSlice appends t to s and returns the result.
-func (s Slice__FIELD_name__) AppendSlice(t Slice) Slice {
-	return append(s, t.(Slice__FIELD_name__)...)
+func (s Slice__FIELD_Name__) AppendSlice(t Slice) Slice {
+	return append(s, t.(Slice__FIELD_Name__)...)
 }
 
 // Cap returns the s capacity.
-func (s Slice__FIELD_name__) Cap() int {
+func (s Slice__FIELD_Name__) Cap() int {
 	return cap(s)
 }
 
 // Copy copies t to s.
-func (s Slice__FIELD_name__) Copy(t Slice) int {
-	return copy(s, t.(Slice__FIELD_name__))
+func (s Slice__FIELD_Name__) Copy(t Slice) int {
+	return copy(s, t.(Slice__FIELD_Name__))
 }
 
 // Equals returns whether s equals v.
-func (s Slice__FIELD_name__) Equals(v interface{}) bool {
-	var t = v.(Slice__FIELD_name__)
+func (s Slice__FIELD_Name__) Equals(v interface{}) bool {
+	var t = v.(Slice__FIELD_Name__)
 	var l = len(s)
 
 	if len(t) != l {
@@ -86,43 +86,43 @@ func (s Slice__FIELD_name__) Equals(v interface{}) bool {
 }
 
 // Get returns the s element at index i.
-func (s Slice__FIELD_name__) Get(i int) interface{} {
+func (s Slice__FIELD_Name__) Get(i int) interface{} {
 	return s[i]
 }
 
 // GetRange returns the slice of s from indexes i to j.
-func (s Slice__FIELD_name__) GetRange(i, j int) Slice {
+func (s Slice__FIELD_Name__) GetRange(i, j int) Slice {
 	return s[i:j]
 }
 
 // GetRangeCap returns the slice of s from indexes i to j with capacity c.
-func (s Slice__FIELD_name__) GetRangeCap(i, j, c int) Slice {
+func (s Slice__FIELD_Name__) GetRangeCap(i, j, c int) Slice {
 	return s[i:j:c]
 }
 
 // Len returns the s length.
-func (s Slice__FIELD_name__) Len() int {
+func (s Slice__FIELD_Name__) Len() int {
 	return len(s)
 }
 
-/// {{if .sort}}
+/// {{if .Sort}}
 // Less implements sort.Interface.
-func (s Slice__FIELD_name__) Less(i, j int) bool {
+func (s Slice__FIELD_Name__) Less(i, j int) bool {
 	return s[i] < s[j]
 } /// {{end}}
 
-// Make returns a new Slice__FIELD_name__ with length l and capacity c.
-func (s Slice__FIELD_name__) Make(l, c int) Slice {
-	return make(Slice__FIELD_name__, l, c)
+// Make returns a new Slice__FIELD_Name__ with length l and capacity c.
+func (s Slice__FIELD_Name__) Make(l, c int) Slice {
+	return make(Slice__FIELD_Name__, l, c)
 }
 
 // Set sets the s element at index i to v.
-func (s Slice__FIELD_name__) Set(i int, v interface{}) {
-	s[i] = v.(__FIELD_element__)
+func (s Slice__FIELD_Name__) Set(i int, v interface{}) {
+	s[i] = v.(__FIELD_Element__)
 }
 
-/// {{if .sort}}
+/// {{if .Sort}}
 // Swap implements sort.Interface.
-func (s Slice__FIELD_name__) Swap(i, j int) {
+func (s Slice__FIELD_Name__) Swap(i, j int) {
 	s[i], s[j] = s[j], s[i]
 } /// {{end}}
