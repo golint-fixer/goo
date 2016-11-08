ae7b358106db9886e1c5c8e7d756d9cfd87d5142
Will Faught <will.faught@bigcommerce.com>
2016-11-07 18:49:32 -0800

    Add Comparable doc

--- comparable.go
+++ comparable.go
@@ -4,11 +4,15 @@ package goo
 type Comparable interface {
 	Equatable
 
+	// Greater returns whether c is less or equal.
 	Greater(c Comparable) bool
 
+	// GreaterEqual returns whether c is less.
 	GreaterEqual(c Comparable) bool
 
+	// Less returns whether c is greater or equal.
 	Less(c Comparable) bool
 
+	// LessEqual returns whether c is greater.
 	LessEqual(c Comparable) bool
 }
