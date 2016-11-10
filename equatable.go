13d8d2296bea027c99f0403d21de9d876570a125
Will Faught <will.faught@bigcommerce.com>
2016-11-09 23:46:11 -0800

    Add Not/Equals

--- equatable.go
+++ equatable.go
@@ -1,5 +1,27 @@
 package goo
 
+// Equal returns whether a and b are equal, using Equatable if possible.
+func Equal(a, b interface{}) bool {
+	if a, ok := a.(Equatable); ok {
+		if b, ok := b.(Equatable); ok {
+			return a.Equals(b)
+		}
+	}
+
+	return a == b
+}
+
+// NotEqual returns whether a and b are not equal, using Equatable if possible.
+func NotEqual(a, b interface{}) bool {
+	if a, ok := a.(Equatable); ok {
+		if b, ok := b.(Equatable); ok {
+			return a.NotEquals(b)
+		}
+	}
+
+	return a != b
+}
+
 // Equatable is equatable to others of the same type.
 type Equatable interface {
 	// Equals returns whether e is equal.
